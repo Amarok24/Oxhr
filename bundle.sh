9dd5621fd571b7e166a46bd7c9fa49966d8eaa82
Jan Prazak <jp.amarok@email.cz>
2021-03-19 17:12:54 +0100

    OOP version 1.0

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@
 echo "Using tsconfig.json for configuration"
 echo "Bundling TS files into one single JS file..."
 
-deno bundle --config ./tsconfig.json src/main.ts dist/bundle.js
+deno bundle --config ./tsconfig.json src/oxhr.ts dist/bundle.js
