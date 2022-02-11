858f265f38479085d73fc0ce1c18c7614732b5d0
Jan Prazak <jp.amarok@email.cz>
2022-02-11 13:03:57 +0100

    minor refactoring

--- src/generate-uuid.ts
+++ src/generate-uuid.ts
@@ -1,32 +1,27 @@
-// This is just a polyfill for the new Crypto API. As of today (Feb 2022) TypeScript type definition files are missing the randomUUID method.
+// As of today (Feb 2022) TypeScript type definition files lack the randomUUID method.
 // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID
+// This is a simple workaround which exports the generateUUID method (ES module).
 
 // TODO: abandon this polyfill as soon as TypeScript gets updated.
 
 export {generateUUID};
-
 
 interface CryptoNew extends Crypto
 {
   randomUUID?(): string;
 }
 
+// Returns an empty string if Crypto API or randomUUID is not supported by browser.
 function generateUUID(): string
 {
-  let cryptoRef: CryptoNew | null = null;
+  let cryptoRef: CryptoNew;
   let r: string | undefined = '';
 
   if (typeof self.crypto !== 'undefined')
   {
-    // Checks if Crypto API exists.
     cryptoRef = self.crypto as CryptoNew;
     r = cryptoRef.randomUUID?.();
   }
 
-  if (!r)
-  {
-    r = 'Crypto API or randomUUID method not supported.';
-  }
-
-  return r;
+  return r ? r : '';
 }
